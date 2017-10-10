b3922b427f1ae26d43a894c0c3c9e99062fb6fbb
Dawid Wrobel <me@dawidwrobel.com>
2017-10-10 12:49:54 -0400

    Reformat setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 
 setup(
- name='csv2ofx.py',
- author = "Dennis Muhlestein",
- version='0.2',
- packages=['csv2ofx.py'],
- package_dir={'csv2ofx.py':'src/csv2ofx.py'},
- scripts=['csv2ofx.py'],
+    name='csv2ofx.py',
+    author="Dennis Muhlestein",
+    version='0.2',
+    packages=['csv2ofx.py'],
+    package_dir={'csv2ofx.py': 'src/csv2ofx.py'},
+    scripts=['csv2ofx.py'],
     package_data={'csv2ofx.py': ['*.xrc']},
     install_requires=['openpyxl']
 )
-
