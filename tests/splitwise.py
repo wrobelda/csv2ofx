1e20f2bbe70937e74b16ee2480500a5bc72591a8
Dawid Wrobel <me@dawidwrobel.com>
2017-10-10 12:48:55 -0400

    Add support for Splitwise statements

---
+++ tests/splitwise.py
@@ -0,0 +1,26 @@
+import unittest
+import filecmp
+from mappings import splitwise
+from csv2ofx import converter
+
+
+class TestSplitwise(unittest.TestCase):
+    infile_name = "Samples\splitwise.csv"
+    outfile_name = "Out\splitwise"
+    bank_name = "Splitwise"
+    splitwise['_params']['owners_name'] = 'owner'
+
+    def test_splitwise(self):
+        c = converter.Converter()
+        encoding = c.GetFileEncoding(self.bank_name)
+
+        c.OpenFile(self.bank_name, self.infile_name)
+        c.ExportFiles(self.bank_name, "QIF", self.outfile_name)
+
+        self.assertTrue(filecmp.cmp("Samples\splitwise Splitwise USD.qif", self.outfile_name + ' Splitwise USD.qif'))
+        self.assertTrue(filecmp.cmp("Samples\splitwise john.doe USD.qif", self.outfile_name + ' john.doe USD.qif'))
+        self.assertTrue(filecmp.cmp("Samples\splitwise owner USD.qif", self.outfile_name + ' owner USD.qif'))
+
+
+if __name__ == '__main__':
+    unittest.main()
