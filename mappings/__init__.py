3cb3d2c386533a9ce53e5d9e0aab9af6fd995cdc
Dawid Wróbel <me@dawidwrobel.com>
2019-07-26 12:18:35 -0400

    update the mappings file after recent mappings additions

--- mappings/__init__.py
+++ mappings/__init__.py
@@ -83,7 +83,7 @@
 
 
 """
-
+from .nestbank import nest
 from .creditunion import cu
 from .msmoneyreport import msmoneyrep
 from .ubs import ubs
@@ -95,9 +95,12 @@ from .venmo import venmo
 from .squarecash import squarecash
 from .paypal import paypal
 from .splitwise import splitwise
+from .eBay import ebay
+from .mbank import mBank_current, mBank_eMakler
 
 all_mappings = {'T-Mobile Konto': tmobilepl, 'Yodlee': yodlee, 'Credit Union': cu, 'UBS': ubs,
                 'MS Money Report (CSV)': msmoneyrep, 'Raiffeisen Polbank Karta': raiffeisenpolbank_ccard,
                 'Raiffeisen Polbank Konto': raiffeisenpolbank_current,
                 'ABN Amro': abnamro, 'Venmo': venmo, 'Square Cash': squarecash, 'PayPal': paypal,
-                'Splitwise': splitwise}
+                    'Splitwise': splitwise, 'eBay': ebay, 'mBank Konto': mBank_current, 'mBank eMakler': mBank_eMakler,
+                'Nest Konto': nest}
